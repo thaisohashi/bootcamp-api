1c0c3de73cc09eb9f284831df72fea6616e625c8
thaisohashi <ohashithais@gmail.com>
2022-07-02 19:37:47 -0300

    deploy settings

--- dist/server.js
+++ dist/server.js
@@ -4,22 +4,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const category_route_1 = require("./routes/category.route");
+const course_route_1 = require("./routes/course.route");
+const environment_variables_1 = require("./config/environment-variables");
+const data_source_1 = require("./config/data-source");
+const middlewares_1 = require("./middlewares");
+const path_1 = require("path");
+const PORT = environment_variables_1.env.PORT || 3000;
 const app = (0, express_1.default)();
-const categories = [
-    {
-        id: 1,
-        name: 'NodeJS',
-        created_at: '2022-05-22 00:00:00',
-        update_at: '2022-05-22 00:00:00'
-    }
-];
-app.get('/', (request, response) => {
-    return response.json({ status: 'sucess', version: '1.0.0 ' }).status(200);
-});
-app.get('/categories', (request, response) => {
-    return response.json(categories).status(200);
-});
-app.listen(3000, () => {
-    console.log('Server in running');
-});
+app.use(express_1.default.json());
+app.use(category_route_1.categoryRoutes, course_route_1.courseRoutes);
+app.use(middlewares_1.errorHandler);
+app.use('/files', express_1.default.static((0, path_1.resolve)(__dirname, '..', 'uploads')));
+data_source_1.AppDataSource.initialize()
+    .then(() => {
+    app.listen(PORT, () => console.log(`Server is running in port: ${PORT}`));
+})
+    .catch((error) => console.log(error));
 //# sourceMappingURL=server.js.map
